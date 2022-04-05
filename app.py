021d57993fd34ccc44c0c7b8cfea698654f6f7b4
ecriswell40 <eric-criswell@uiowa.edu>
2022-04-04 19:13:41 -0500

    update app.py

--- app.py
+++ app.py
@@ -4,12 +4,16 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def Home():
     return render_template('index.html')
 
-@app.route('/eric')
-def eric():
-    return render_template('eric.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/estimate')
+def estimate():
+    return render_template('estimate.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
