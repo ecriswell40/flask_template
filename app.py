e35b9d2905259bfb41f3bc85e605a3e25e80a0f3
ecriswell40 <eric-criswell@uiowa.edu>
2022-04-26 14:21:57 -0500

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
@@ -11,29 +11,14 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=['GET', 'POST'])
 def estimate():
-   # def calculate_tank_top(tank_top):
-       # tank_top= 3.14*(tankradius)
-
-    #def calculate_tank_sides(tank_sides):
-       # tank_sides=2(3.14([tankradius]*[tankheight]))
-
-   # def calculate_total_area_in(total_area_in):
-      #  tank_area_in=tank_top + tank_sides
-
-   # def calculate_total_sqft(total_sqft):
-       # total_sqft=total_area_in/144
-
-   # def calculate_material_cost(material_cost):
-      #  material_cost=total_sqft*25
-
-   # def calculate_labor_cost(labor_cost):
-      #  labor_cost=total_sqft*15
-
-   # def calculate_total_cost(total_cost):
-      #  total_cost=material_cost + labor_cost
-      #  return (Estimated total cost would be (total_cost))
+    if request.method == 'POST':
+        form = request.form
+        radius = float (form['radius'])
+        height = float (form['height'])
+        estimate=radius+height
+        return render_template('estimate.html', data=estimate)
     return render_template('estimate.html')
 
 if __name__ == '__main__':
