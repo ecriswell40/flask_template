7250a126ebef8ca0639fea10f73a9e0e7dd6f716
ecriswell40 <eric-criswell@uiowa.edu>
2022-04-26 13:51:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,27 +13,27 @@ def about():
 
 @app.route('/estimate')
 def estimate():
-    def calculate_tank_top(tank_top):
-        tank_top= 3.14*(tankradius)
+   # def calculate_tank_top(tank_top):
+       # tank_top= 3.14*(tankradius)
 
-    def calculate_tank_sides(tank_sides):
-        tank_sides=2(3.14([tankradius]*[tankheight]))
+    #def calculate_tank_sides(tank_sides):
+       # tank_sides=2(3.14([tankradius]*[tankheight]))
 
-    def calculate_total_area_in(total_area_in):
-        tank_area_in=tank_top + tank_sides
+   # def calculate_total_area_in(total_area_in):
+      #  tank_area_in=tank_top + tank_sides
 
-    def calculate_total_sqft(total_sqft):
-        total_sqft=total_area_in/144
+   # def calculate_total_sqft(total_sqft):
+       # total_sqft=total_area_in/144
 
-    def calculate_material_cost(material_cost):
-        material_cost=total_sqft*25
+   # def calculate_material_cost(material_cost):
+      #  material_cost=total_sqft*25
 
-    def calculate_labor_cost(labor_cost):
-        labor_cost=total_sqft*15
+   # def calculate_labor_cost(labor_cost):
+      #  labor_cost=total_sqft*15
 
-    def calculate_total_cost(total_cost):
-        total_cost=material_cost + labor_cost
-        return (Estimated total cost would be (total_cost))
+   # def calculate_total_cost(total_cost):
+      #  total_cost=material_cost + labor_cost
+      #  return (Estimated total cost would be (total_cost))
     return render_template('estimate.html')
 
 if __name__ == '__main__':
