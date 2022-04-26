de1b25685038c810939f4025173c3a6a37b07636
ecriswell40 <eric-criswell@uiowa.edu>
2022-04-26 16:20:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,17 +17,17 @@ def estimate():
         form = request.form
         radius = float (form['radius'])
         height = float (form['height'])
-    pi = 3.14
-    labor_cost = 15
-    material_cost = 25
-    area_of_tank_top = pi * 'radius'
-    area_of_tank_sides = 2(pi(radius*height))
-    total_area = area_of_tank_top + area_of_tank_sides
-    total_sqft = total_area/144
-    total_material_cost = total_sqft * material_cost
-    total_labor_cost = total_sqft * labor_cost
-    estimate = total_material_cost + total_labor_cost
-    return render_template('estimate.html', data=estimate)
+        pi = 3.14
+        labor_cost = 15
+        material_cost = 25
+        area_of_tank_top = pi *radius
+        area_of_tank_sides = 2(pi(radius*height))
+        total_area = area_of_tank_top + area_of_tank_sides
+        total_sqft = total_area/144
+        total_material_cost = total_sqft * material_cost
+        total_labor_cost = total_sqft * labor_cost
+        estimate = total_material_cost + total_labor_cost
+        return render_template('estimate.html', data=estimate)
     return render_template('estimate.html')
 
 if __name__ == '__main__':
