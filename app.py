1504a8fa3f23169e2f00f756ddc9b46256ef694f
ecriswell40 <eric-criswell@uiowa.edu>
2022-04-26 16:22:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import render_template, request
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return render_template('index.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
